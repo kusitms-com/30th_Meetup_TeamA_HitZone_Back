0eb6f50dff2e4767cafa46f852a3ff5f84d3fa2d
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-10-14 18:06:41 +0900

    #11 [feat] 스타디움 도메인의 에러, 성공 커스텀 status를 구현한다.

---
+++ src/main/java/kusitms/backend/stadium/status/StadiumErrorStatus.java
@@ -0,0 +1,37 @@
+package kusitms.backend.stadium.status;
+
+import kusitms.backend.global.code.BaseErrorCode;
+import kusitms.backend.global.dto.ErrorReasonDto;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum StadiumErrorStatus implements BaseErrorCode {
+    _NOT_FOUND_STADIUM(HttpStatus.NOT_FOUND, "8201", "존재하지 않는 스타디움명입니다. 다시 입력해주세요."),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ErrorReasonDto getReason() {
+        return ErrorReasonDto.builder()
+                .isSuccess(false)
+                .code(code)
+                .message(message)
+                .build();
+    }
+
+    @Override
+    public ErrorReasonDto getReasonHttpStatus() {
+        return ErrorReasonDto.builder()
+                .isSuccess(false)
+                .httpStatus(httpStatus)
+                .code(code)
+                .message(message)
+                .build();
+    }
+}
