59bd9c37623e487503f3ccd99b0e5d8042965402
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-10-14 17:58:19 +0900

    #11 [style] Reference, ReferencesGroup 필드명 변경

--- src/main/java/kusitms/backend/stadium/common/Reference.java
+++ src/main/java/kusitms/backend/stadium/common/Reference.java
@@ -8,6 +8,6 @@ import java.util.List;
 @Getter
 @RequiredArgsConstructor
 public class Reference {
-    private final String title;
+    private final String referenceTitle;
     private final List<String> contents;
 }
