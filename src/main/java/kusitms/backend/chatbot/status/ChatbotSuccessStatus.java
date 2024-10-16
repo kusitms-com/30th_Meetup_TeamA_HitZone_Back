6e49da62cc690ac4c5206aab4a564e0017a8e3dd
bbbang105 <2018111366@dgu.ac.kr>
2024-10-16 13:40:36 +0900

    #15 [feat] : 챗봇 성공 & 실패 응답 enum을 추가한다

---
+++ src/main/java/kusitms/backend/chatbot/status/ChatbotSuccessStatus.java
@@ -0,0 +1,38 @@
+package kusitms.backend.chatbot.status;
+
+import kusitms.backend.global.code.BaseCode;
+import kusitms.backend.global.dto.ReasonDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ChatbotSuccessStatus implements BaseCode {
+
+    _GET_GUIDE_CHATBOT_ANSWER(HttpStatus.OK, "200", "가이드 챗봇 답변을 가져오는 데 성공했습니다.")
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ReasonDto getReason() {
+        return ReasonDto.builder()
+                .isSuccess(true)
+                .code(code)
+                .message(message)
+                .build();
+    }
+
+    @Override
+    public ReasonDto getReasonHttpStatus() {
+        return ReasonDto.builder()
+                .isSuccess(true)
+                .httpStatus(httpStatus)
+                .code(code)
+                .message(message)
+                .build();
+    }
+}
