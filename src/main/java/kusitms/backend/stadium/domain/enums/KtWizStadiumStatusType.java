058828b3a465b8ae01aca84e9615367c41660a74
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-10-16 18:12:39 +0900

    #11 [chore] StadiumStatusType 잠실, kt위즈의 참고사항 마지막 줄은 \n 삽입이 안되게 변경한다.

--- src/main/java/kusitms/backend/stadium/domain/enums/KtWizStadiumStatusType.java
+++ src/main/java/kusitms/backend/stadium/domain/enums/KtWizStadiumStatusType.java
@@ -24,15 +24,13 @@ public enum KtWizStadiumStatusType implements StadiumStatusType{
                                                     1루 ‘홈팀’ KT 위즈의 응원단상은
                                                     109, 110 구역에 위치해요.
                                                     3루 ‘원정팀’ 원정 응원단상은
-                                                    121, 122 구역에 위치해요.
-                                                    """
+                                                    121, 122 구역에 위치해요."""
                                     ),
                                     new Reference(
                                             "시끄러운 것을 좋아하지 않는 분",
                                             """
                                                     해당 구역 앞에는 응원을 위한 앰프가 설치되어 있어요.
-                                                    조용한 관람을 원하시면 다른 구역을 추천해요!
-                                                    """
+                                                    조용한 관람을 원하시면 다른 구역을 추천해요!"""
                                     )
                             )
                     )
@@ -54,8 +52,7 @@ public enum KtWizStadiumStatusType implements StadiumStatusType{
                                             "전용 게이트, 전용 팔찌",
                                             """
                                             3루 1게이트로 입장 후, 전용 게이트가 있어요.
-                                            전용 팔찌를 채워주셔서, 재입장 시 편리해요.
-                                            """
+                                            전용 팔찌를 채워주셔서, 재입장 시 편리해요."""
                                     ),
                                     new Reference(
                                             "넓은 좌석 간격",
@@ -83,8 +80,7 @@ public enum KtWizStadiumStatusType implements StadiumStatusType{
                                             "타 팀 팬들과 함께",
                                             """
                                                     동행자가 타 팀일 때 가기 좋아요.
-                                                    어느 한 팀에 치우쳐져서 응원하는 분위기가 아니예요!
-                                                    """
+                                                    어느 한 팀에 치우쳐져서 응원하는 분위기가 아니예요!"""
                                     )
                             )
                     )
@@ -109,8 +105,7 @@ public enum KtWizStadiumStatusType implements StadiumStatusType{
                                             "전용 게이트, 팔찌",
                                             """
                                                     Y박스석은 전용 게이트를 이용할 수 있어요!
-                                                    전용 팔찌를 채워주셔서, 재입장 시 편리해요.
-                                                    """
+                                                    전용 팔찌를 채워주셔서, 재입장 시 편리해요."""
                                     )
                             )
                     )
@@ -135,16 +130,14 @@ public enum KtWizStadiumStatusType implements StadiumStatusType{
                                             "별도의 티켓 부스",
                                             """
                                                     예약 정보를 확인한 뒤 티켓을 수령하시고,
-                                                    전용 엘레베이터를 타고 올라갈 수 있어요.
-                                                    """
+                                                    전용 엘레베이터를 타고 올라갈 수 있어요."""
                                     ),
                                     new Reference(
                                             "배달 주문시 꿀팁",
                                             """
                                                     주소를 수원KT위즈파크로 설정하신 후
                                                     상세 주소에 ‘n번 엘레베이터 앞’으로 기재하시면
-                                                    편하게 음식을 픽업하실 수 있어요!
-                                                    """
+                                                    편하게 음식을 픽업하실 수 있어요!"""
                                     )
                             )
                     )
@@ -166,16 +159,14 @@ public enum KtWizStadiumStatusType implements StadiumStatusType{
                                             """
                                                     높은 시야에서 야구를 관람할 수 있기 때문에,
                                                     홈과 가까운 시야에서 관람을 원하시면
-                                                    다른 구역을 추천해요!
-                                                    """
+                                                    다른 구역을 추천해요!"""
                                     ),
                                     new Reference(
                                             "높은 곳까지 걸어 오르기 힘드신 분",
                                             """
                                                     높은 층수에 위치해 있기 때문에
                                                     좌석을 찾아가기까지 걸어오르는 과정이 필요해요.
-                                                    무릎이나 체력이 안 좋으시면 비추천해요!
-                                                    """
+                                                    무릎이나 체력이 안 좋으시면 비추천해요!"""
                                     )
                             )
                     )
@@ -199,23 +190,20 @@ public enum KtWizStadiumStatusType implements StadiumStatusType{
                                             "승리 시 선수와 하이파이브",
                                             """
                                                     미리 중앙 빅또리센터로 가서 번호표를 수령해야,
-                                                    경기 승리 시 선수들과 하이파이브를 할 수 있어요.
-                                                    """
+                                                    경기 승리 시 선수들과 하이파이브를 할 수 있어요."""
                                     ),
                                     new Reference(
                                             "전용 팔찌",
                                             """
                                                     1루 하이파이브존 입장 전,
                                                     한 번 더 티켓 검사를 진행한 후 ‘입장 팔찌’를 채워주세요.
-                                                    이 팔찌가 있어야 재입장을 할 수 있으니 유의하세요!
-                                                    """
+                                                    이 팔찌가 있어야 재입장을 할 수 있으니 유의하세요!"""
                                     ),
                                     new Reference(
                                             "불펜 투수 연습 관람",
                                             """
                                                     ‘오른쪽’으로 예매를 하시면 불펜 투수들이
-                                                    연습하는 모습을 가깝게 보실 수 있어요.
-                                                    """
+                                                    연습하는 모습을 가깝게 보실 수 있어요."""
                                     )
                             )
                     ),
@@ -227,8 +215,7 @@ public enum KtWizStadiumStatusType implements StadiumStatusType{
                                             """
                                                     3루 하이파이브존 입장 전,
                                                     한 번 더 티켓 검사를 진행한 후 ‘입장 팔찌’를 채워주세요.
-                                                    이 팔찌가 있어야 재입장을 할 수 있으니 유의하세요!
-                                                    """
+                                                    이 팔찌가 있어야 재입장을 할 수 있으니 유의하세요!"""
                                     )
                             )
                     )
@@ -250,10 +237,7 @@ public enum KtWizStadiumStatusType implements StadiumStatusType{
                                             """
                                                     매진이 빠른 편이라 가고 싶다면
                                                     예매 일자가 열리는 날을 체크해둔 후
-                                                    예매하는 것이 좋아요!
-                                                    """
-
-
+                                                    예매하는 것이 좋아요!"""
                                     ),
                                     new Reference(
                                             "가까운 게이트",
@@ -284,8 +268,7 @@ public enum KtWizStadiumStatusType implements StadiumStatusType{
                                                     구단마다 포수 뒤 관람 공간을 부르는 용어가 상이한데,
                                                     위즈 파크의 경우 ‘지니존’이에요.
                                                     좌/중/우 세 부분으로 나뉘고
-                                                    각각 5열로 구성되어 있어요.
-                                                    """
+                                                    각각 5열로 구성되어 있어요."""
                                     )
                             )
                     )
@@ -329,15 +312,13 @@ public enum KtWizStadiumStatusType implements StadiumStatusType{
                                             """
                                                     관중이 몰리는 날에는,
                                                     야구장에 일찍 가서 자리를 잡는 것이 좋아요.
-                                                    ‘2시간 전’부터 입장 가능해요!
-                                                    """
+                                                    ‘2시간 전’부터 입장 가능해요!"""
                                     ),
                                     new Reference("돗자리 필수",
                                             """
                                                     다른 구역처럼 좌석이 있는 것이 아니라,
                                                     돗자리나 미니 테이블을 가져오면
-                                                    편리하게 관람하실 수 있어요!
-                                                    """
+                                                    편리하게 관람하실 수 있어요!"""
                                     )
                             )
                     )
@@ -362,15 +343,13 @@ public enum KtWizStadiumStatusType implements StadiumStatusType{
                                                     신분증을 가지고 3층
                                                     ‘키즈랜드 캠핑 운영사무국’으로 가시면 캠핑용품을
                                                     무료로 대여할 수 있어요!
-                                                    5층 캠핑존에 전자레인지와 셀프 라면기도 있어요
-                                                    """
+                                                    5층 캠핑존에 전자레인지와 셀프 라면기도 있어요"""
                                     ),
                                     new Reference(
                                             "아이들과 함께",
                                             """
                                                     아이들이 있어야 입장 가능한 구역이예요.
-                                                    (최대 4인까지 입장 가능해요)
-                                                    """
+                                                    (최대 4인까지 입장 가능해요)"""
                                     )
                             )
                     )
