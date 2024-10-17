3d67cc947096bfad12e48c460aa3ac5f36efce15
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-10-17 23:17:48 +0900

    #19 [feat] 추천받은 구역 저장 api 구현을 완료하여 resultId를 반환한다.

--- src/main/java/kusitms/backend/stadium/application/StadiumService.java
+++ src/main/java/kusitms/backend/stadium/application/StadiumService.java
@@ -3,6 +3,7 @@ package kusitms.backend.stadium.application;
 import kusitms.backend.global.exception.CustomException;
 import kusitms.backend.stadium.common.RecommendedUserProfile;
 import kusitms.backend.stadium.common.RecommendedTopRankedZones;
+import kusitms.backend.stadium.domain.entity.Profile;
 import kusitms.backend.stadium.domain.entity.Zone;
 import kusitms.backend.stadium.domain.entity.Result;
 import kusitms.backend.stadium.domain.entity.Stadium;
@@ -12,6 +13,7 @@ import kusitms.backend.stadium.domain.enums.ProfileStatusType;
 import kusitms.backend.stadium.domain.enums.StadiumStatusType;
 import kusitms.backend.stadium.domain.repository.ResultRepository;
 import kusitms.backend.stadium.domain.repository.StadiumRepository;
+import kusitms.backend.stadium.domain.repository.ZoneRepository;
 import kusitms.backend.stadium.dto.request.SaveTopRankedZoneRequestDto;
 import kusitms.backend.stadium.dto.response.SaveTopRankedZoneResponseDto;
 import kusitms.backend.stadium.status.StadiumErrorStatus;
@@ -29,6 +31,7 @@ public class StadiumService {
     private final UserRepository userRepository;
     private final StadiumRepository stadiumRepository;
     private final ResultRepository resultRepository;
+    private final ZoneRepository zoneRepository;
 
     @Transactional
     public <T extends Enum<T> & StadiumStatusType> SaveTopRankedZoneResponseDto recommendZones(SaveTopRankedZoneRequestDto request) {
@@ -36,7 +39,7 @@ public class StadiumService {
         T[] zones = switch (request.stadium()) {
             case "잠실종합운동장" -> (T[]) JamsilStadiumStatusType.values();
             case "수원KT위즈파크" -> (T[]) KtWizStadiumStatusType.values();
-            default -> throw new CustomException(StadiumErrorStatus._BAD_REQUEST_STADIUM);
+            default -> throw new CustomException(StadiumErrorStatus._NOT_FOUND_STADIUM);
         };
 
         ProfileStatusType recommendedProfile = RecommendedUserProfile.getRecommendedUserProfile(
@@ -44,22 +47,32 @@ public class StadiumService {
         List<T> recommendZones = RecommendedTopRankedZones.getTopRankedZones(
                 zones, List.of(request.clientKeywords()));
 
-        Stadium stadium = stadiumRepository.findByName(request.stadium());
+        Stadium stadium = stadiumRepository.findByName(request.stadium())
+                .orElseThrow(() -> new CustomException(StadiumErrorStatus._NOT_FOUND_STADIUM));
         Result result = Result.builder()
                 .stadium(stadium)
                 .preference(request.preference())
-                .profile(recommendedProfile)
                 .build();
         resultRepository.save(result);
+
+        Profile profile = Profile.builder()
+                .result(result)
+                .nickname(recommendedProfile.getNickName())
+                .type(recommendedProfile.getType())
+                .explanation(recommendedProfile.getExplanation())
+                .hashTags(recommendedProfile.getHastTags())
+                .build();
 
        recommendZones.forEach(zoneEnum -> {
            Zone zone = Zone.builder()
                    .result(result)
-                   .zone((StadiumStatusType) zoneEnum)
+                   .name(zoneEnum.getZoneName())
+                   .explanations(zoneEnum.getExplanations())
+                   .tip(zoneEnum.getTip())
+                   .referencesGroup(zoneEnum.getReferencesGroup())
                    .build();
+           zoneRepository.save(zone);
        });
-
        return SaveTopRankedZoneResponseDto.from(result.getId());
-
     }
 }
--- src/main/java/kusitms/backend/stadium/status/StadiumErrorStatus.java
+++ src/main/java/kusitms/backend/stadium/status/StadiumErrorStatus.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum StadiumErrorStatus implements BaseErrorCode {
-    _BAD_REQUEST_STADIUM(HttpStatus.BAD_REQUEST, "8201", "해당하는 스타디움이 존재하지 않습니다. 구장이름을 다시 한번 확인해주세요."),
+    _NOT_FOUND_STADIUM(HttpStatus.NOT_FOUND, "8201", "해당하는 스타디움이 존재하지 않습니다. 구장이름을 다시 한번 확인해주세요."),
     ;
 
     private final HttpStatus httpStatus;
