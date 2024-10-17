f27e5063bb25b7b3a2735d5a4a44d40c2c1d5d23
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-10-17 15:34:27 +0900

    #19 [feat] 추천받은 구역 RecommendedZone 엔티티를 생성한다.

---
+++ src/main/java/kusitms/backend/stadium/domain/entity/RecommendedZone.java
@@ -0,0 +1,26 @@
+package kusitms.backend.stadium.domain.entity;
+
+import jakarta.persistence.*;
+import kusitms.backend.stadium.domain.enums.ProfileStatusType;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class RecommendedZone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "zone_id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "result_id", nullable = false)
+    private Result result;
+
+    @Column(nullable = false)
+    private ProfileStatusType zone;
+
+}
