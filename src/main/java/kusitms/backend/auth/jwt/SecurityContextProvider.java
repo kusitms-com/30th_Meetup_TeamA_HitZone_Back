b3f3becb1b612059eb24ea9481b4d7503bb91e60
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-10-18 15:45:35 +0900

    #19 [feat] 익명 사용자 유무 확인 메소드를 삭제한다.

--- src/main/java/kusitms/backend/auth/jwt/SecurityContextProvider.java
+++ src/main/java/kusitms/backend/auth/jwt/SecurityContextProvider.java
@@ -14,25 +14,11 @@ public class SecurityContextProvider {
 
     public static Long getAuthenticatedUserId() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || !authentication.isAuthenticated() || isAnonymousUser()) {
-            return null;
-        }
         Object principal = authentication.getPrincipal();
         if (Objects.isNull(principal)) {
             throw new CustomException(AuthErrorStatus._NOT_AUTHENTICATED);
         }
         return (Long) principal;
-    }
-
-    // anonymousUser 확인 메소드
-    public static boolean isAnonymousUser() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || authentication.getAuthorities() == null) {
-            return false;
-        }
-        return authentication.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .anyMatch(role -> role.equals("ROLE_ANONYMOUS"));
     }
 
 // 테스트를 위한 코드
