46948040a963ab2295e1609dfa6a020eca677df3
bbbang105 <2018111366@dgu.ac.kr>
2024-10-19 11:45:20 +0900

    #16 [feat] : 메세지 생성 팩토리를 추가한다

---
+++ src/main/java/kusitms/backend/chatbot/application/MessageFactory.java
@@ -0,0 +1,30 @@
+package kusitms.backend.chatbot.application;
+
+import kusitms.backend.chatbot.domain.enums.Role;
+import kusitms.backend.chatbot.dto.request.Message;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Base64;
+
+@Component
+public class MessageFactory {
+
+    @Value("${clova.prompt.baseball}")
+    private String baseballPrompt;
+
+    // 사용자 메시지 생성
+    public Message createUserMessage(String content) {
+        return new Message(Role.user, content);
+    }
+
+    // 시스템 메시지 생성
+    public Message createSystemMessage() {
+        return new Message(Role.system, new String(Base64.getDecoder().decode(baseballPrompt)));
+    }
+
+    // 어시스턴트 메시지 생성
+    public Message createAssistantMessage(String content) {
+        return new Message(Role.assistant, content);
+    }
+}
