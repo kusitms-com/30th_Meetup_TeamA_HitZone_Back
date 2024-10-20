d034a079f3b50da517161355ed5f735fe5534bf9
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-10-20 15:49:43 +0900

    #21 [feat] 구역가이드 API 비즈니스 로직 구현 완료.

--- src/main/java/kusitms/backend/stadium/application/StadiumService.java
+++ src/main/java/kusitms/backend/stadium/application/StadiumService.java
@@ -1,7 +1,43 @@
 package kusitms.backend.stadium.application;
 
+import kusitms.backend.global.exception.CustomException;
+import kusitms.backend.result.domain.enums.JamsilStadiumStatusType;
+import kusitms.backend.result.domain.enums.KtWizStadiumStatusType;
+import kusitms.backend.result.domain.enums.StadiumStatusType;
+import kusitms.backend.stadium.domain.entity.Stadium;
+import kusitms.backend.stadium.domain.repository.StadiumRepository;
+import kusitms.backend.stadium.dto.response.GetZoneGuideResponseDto;
+import kusitms.backend.stadium.status.StadiumErrorStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
 
 @Service
+@RequiredArgsConstructor
 public class StadiumService {
+
+    private final StadiumRepository stadiumRepository;
+
+    @Transactional(readOnly = true)
+    public GetZoneGuideResponseDto getZoneGuide(String stadiumName, String zoneName) {
+        Stadium stadium = stadiumRepository.findByName(stadiumName)
+                .orElseThrow(() -> new CustomException(StadiumErrorStatus._NOT_FOUND_STADIUM));
+
+        StadiumStatusType zoneType = switch (stadiumName) {
+            case "잠실종합운동장" -> findZoneInStadium(JamsilStadiumStatusType.values(), zoneName);
+            case "수원KT위즈파크" -> findZoneInStadium(KtWizStadiumStatusType.values(), zoneName);
+            default -> throw new CustomException(StadiumErrorStatus._NOT_FOUND_STADIUM);
+        };
+
+        return GetZoneGuideResponseDto.from(zoneType);
+    }
+
+    private StadiumStatusType findZoneInStadium(StadiumStatusType[] statusTypes, String zoneName) {
+        return Arrays.stream(statusTypes)
+                .filter(status -> status.getZoneName().equals(zoneName))
+                .findFirst()
+                .orElseThrow(() -> new CustomException(StadiumErrorStatus._NOT_FOUND_ZONE));
+    }
 }
