042bf9f967347844e66ce0f131140ba59a35357d
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-11-06 16:44:28 +0900

    #46 [feat] 먹거리 Food 엔티티의 필드를 모두 설정한다.

--- src/main/java/kusitms/backend/food/domain/entity/Food.java
+++ src/main/java/kusitms/backend/food/domain/entity/Food.java
@@ -3,10 +3,14 @@ package kusitms.backend.food.domain.entity;
 import jakarta.persistence.*;
 import kusitms.backend.food.domain.enums.Boundary;
 import kusitms.backend.food.domain.enums.Course;
+import kusitms.backend.result.domain.converter.StringListConverter;
 import kusitms.backend.stadium.domain.entity.Stadium;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -28,6 +32,31 @@ public class Food {
     @Enumerated(EnumType.STRING)
     private Course course;
 
+    @Column(nullable = false)
+    private String name;
 
+    @Column(nullable = false)
+    private String location;
 
+    @Lob
+    @Convert(converter = StringListConverter.class)
+    private List<String> menu;
+
+    @Column(nullable = false)
+    private String price;
+
+    @Column(nullable = false)
+    private String explanation;
+
+    @Builder
+    public Food(Stadium stadium, String boundary, String course, String name, String location, List<String> menu, String price, String explanation) {
+        this.stadium = stadium;
+        this.boundary = Boundary.of(boundary);
+        this.course = Course.of(course);
+        this.name = name;
+        this.location = location;
+        this.menu = menu;
+        this.price = price;
+        this.explanation = explanation;
+    }
 }
