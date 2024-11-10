1ca0d23e0493177054d592aa236978e134231971
bbbang105 <2018111366@dgu.ac.kr>
2024-11-11 00:48:04 +0900

    #70 [feat] : 임시 클라이언트 배포 도메인 cors를 허용한다

--- src/main/java/kusitms/backend/global/config/SecurityConfig.java
+++ src/main/java/kusitms/backend/global/config/SecurityConfig.java
@@ -62,7 +62,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000", "https://git.hitzone.store"));
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:5173",
+                "http://localhost:3000",
+                "https://git.hitzone.store",
+                "https://frontenddev-t5wq.vercel.app"));
         configuration.setAllowedMethods(Collections.singletonList("*"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(Collections.singletonList("*"));
